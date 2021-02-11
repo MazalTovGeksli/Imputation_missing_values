78b3ecaeb0e7a7f3ea9fb515aa6fd0a7001595f0
Александр <kaa1994g@mail.ru>
2021-02-11 14:06:50 +0700

    fix edge case handling

--- Metrics/Metrics.py
+++ Metrics/Metrics.py
@@ -1,32 +1,44 @@
 import pandas as pd
 import numpy as np
 
-def NRMSE(actual, missing, imputation, cat_feature):
+def NRMSE(actual, missing, imputation, cat_feature = None):
+  if cat_feature != None:
+    if not bool(cat_feature):
+      raise ValueError("Пустой список индексов категориальных признаков")
+    list_num_feature = np.delete(imputation.columns, cat_feature, 0)
+  else:
+    list_num_feature = imputation.columns
+  
   mask = missing.isna()
   actual = actual[mask]
   imputation = imputation[mask]
   ximp = []
   xtrue = []
-  for i in np.delete(imputation.columns, cat_feature, 0):
+  for i in list_num_feature:
     ximp = ximp + imputation[i].dropna().to_list()
     xtrue = xtrue + actual[i].dropna().to_list()
   res = np.sqrt(np.mean((np.array(ximp) - np.array(xtrue)) ** 2) / np.var(np.array(xtrue)))
-  return res
+  return {'NRMSE': res}
 
-def PFC(actual, missing, imputation, cat_feature):
-  mask = missing.isna()
-  actual = actual[mask]
-  imputation = imputation[mask]
-  sum_na = 0
-  sum_error = 0
-  for i in imputation.columns[cat_feature]:
-    sum_na = sum_na + mask[i].sum()
-    res = 0
-    for k, j in zip(imputation[i].dropna().to_list(), actual[i].dropna().to_list()):
-      if (k != j):
-        res = res + 1
-    sum_error = sum_error + res
-  return sum_error/sum_na
-
-def mixError(ximp, xmis, xtrue, cat_feature):
-  return {'NRMSE': NRMSE(ximp, xmis, xtrue, cat_feature), 'PFC': PFC(ximp, xmis, xtrue, cat_feature)}
+def PFC(actual, missing, imputation, cat_feature = None):
+  if cat_feature != None:
+    if not bool(cat_feature):
+      raise ValueError("Пустой список индексов категориальных признаков")
+    mask = missing.isna()
+    actual = actual[mask]
+    imputation = imputation[mask]
+    sum_na = 0
+    sum_error = 0
+    for i in imputation.columns[cat_feature]:
+      sum_na = sum_na + mask[i].sum()
+      res = 0
+      for k, j in zip(imputation[i].dropna().to_list(), actual[i].dropna().to_list()):
+        if (k != j):
+          res = res + 1
+      sum_error = sum_error + res
+    return {'PFC': sum_error/sum_na}
+  else:
+    return np.nan
+  
+def mixError(ximp, xmis, xtrue, cat_feature = None):
+  return [NRMSE(ximp, xmis, xtrue, cat_feature), PFC(ximp, xmis, xtrue, cat_feature)]
