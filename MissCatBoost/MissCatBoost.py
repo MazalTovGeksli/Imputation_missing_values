1613c028890bc30769ec391ec71cdd5cc82c9977
Александр <kaa1994g@mail.ru>
2021-03-02 11:09:55 +0700

    class misscatboost

--- MissCatBoost/MissCatBoost.py
+++ MissCatBoost/MissCatBoost.py
@@ -1,103 +1,160 @@
+from catboost import Pool, CatBoostClassifier, CatBoostRegressor
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
-from catboost import Pool, CatBoostClassifier, CatBoostRegressor
-import Imputation_missing_values.Metrics.Metrics as metrics
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-def initial_missing_value(data, cat_vars):
-  col_cat = data.columns[cat_vars]
-  col_num = np.delete(data.columns, cat_vars, 0)
+class MissCatBoost():
 
-  res1 = initial_missing_categorical_value(data, col_cat)
-  res2 = initial_missing_numeric_value(data, col_num)
-  data[col_cat] = res1
-  data[col_num] = res2
-  return data
+  def __init__(self, max_iter=10, missing_values=np.nan, n_estimators=100, depth=6, criterion=('RMSE', 'MultiClass')):
+    self.max_iter = max_iter
+    self.missing_values = missing_values
+    self.n_estimators = n_estimators
+    self.depth = depth
+    self.criterion = criterion
+    self.iter_list = []
+    self.reg_score_list = []
+    self.class_score_list = []
 
-def initial_missing_numeric_value(data, col_num):
-  sumple_imp = SimpleImputer(missing_values = np.nan, strategy = 'median')
-  res2 = pd.DataFrame(sumple_imp.fit_transform(data[col_num]), columns = data[col_num].columns)
-  return res2
-
-def initial_missing_categorical_value(data, col_cat):
-  sumple_imp = SimpleImputer(missing_values = np.nan, strategy = 'most_frequent')
-  res1 = pd.DataFrame(sumple_imp.fit_transform(data[col_cat]), columns = data[col_cat].columns)
-  return res1
-
-def sort_missing_columns(data, mask, ascending=False):
-  return data[mask.sum().sort_values(ascending=ascending).index]
-
-"""
-Parameters
-----------
-cat_vars : list, None (default =  None)
-  Список индексов признаков, являющихся категориальными.
-"""
-
-def miss_catboost(data, actual = None, cat_vars = None, iter = 5, iterations = 50, sort = None):
-  if actual == None:
-    miss_data = None
-  else:
-    miss_data = data.copy()
-  
-  mask = data.isna()
-  if np.any(mask.sum(axis=0) >= (data.shape[0])):
-    raise ValueError("One or more columns have all rows missing.") # Check if any column has all missing
-
-  if cat_vars != None:
-    cat_vars_names = data.columns[cat_vars]
-  else:
-    cat_vars_names = []
-  
-  if sort != None:                                                              # сортировка пока что не понятно зачем нужна. С ней больше мороки
-    data = sort_missing_columns(data, mask, ascending=sort)
-    cat_vars = [data.columns.get_loc(c) for c in cat_vars_names if c in data]
-    cat_vars_names = data.columns[cat_vars]
-  
-  data = initial_missing_value(data, cat_vars)
-
-  model_reg = CatBoostRegressor(verbose = False, iterations= iterations)
-  model_class = CatBoostClassifier(verbose = False, iterations= iterations, loss_function = 'MultiClass')
-
-  print(cat_vars_names)
-
-  err_all = np.inf
-  while iter > 0:
-    error_class = 0
-    error_reg = 0
-
-    for i in data.columns:
-      test = data[mask[i]]
-      train = data.drop(test.index, axis = 0)
-
-      y_test = test[i].reset_index(drop = True)
-      y_train = train[i]
-      X_test = test.drop([i], axis = 1)
-      X_train = train.drop([i], axis = 1)
-
-      list_cf = list([X_train.columns.get_loc(c) for c in cat_vars_names if c in X_train])
-      if i in cat_vars_names:
-        model_class.fit(X_train, y_train, cat_features = list_cf)
-        y_test = model_class.predict(X_test)
-        y_test = np.reshape(y_test, (len(y_test)))
-
-        error_class = error_class + model_class.evals_result_['learn']['MultiClass'][iterations - 1]
-      else:
-        model_reg.fit(X_train, y_train, cat_features = list_cf)
-        y_test = model_reg.predict(X_test)
-
-        error_reg = error_reg + model_reg.evals_result_['learn']['RMSE'][iterations - 1] / np.std(y_test)
-
-      data[i][mask[i]] = y_test
-
-    if (error_reg + error_class)/2 < err_all:
-      print({'err_all': err_all, 'sum(error_reg + error_class)': (error_reg + error_class)/2})
-      err_all = (error_reg + error_class)/2
+  def _initial_missing_value(self, data, cat_vars = None):
+    if cat_vars == None:
+      data[data.columns] = self._initial_missing_numeric_value(data, data.columns)
     else:
-      print('ПРЕРЫВАНИЕ ЦИКЛА')
-      print({'err_all': err_all, 'sum(error_reg + error_class)': (error_reg + error_class)/2})
-      break
+      if not bool(cat_vars):
+        raise ValueError("Пустой список индексов категориальных признаков")
 
-    iter = iter - 1
+      col_cat = data.columns[cat_vars]
+      col_num = np.delete(data.columns, cat_vars, 0)
 
-  return(data)
+      if len(col_num) == 0:
+        data[col_cat] = self._initial_missing_categorical_value(data, col_cat)
+      else:
+        data[col_cat] = self._initial_missing_categorical_value(data, col_cat)
+        data[col_num] = self._initial_missing_numeric_value(data, col_num)
+
+    return data
+
+  def _initial_missing_numeric_value(self, data, col_num):
+    sumple_imp = SimpleImputer(missing_values = np.nan, strategy = 'median')
+    res2 = pd.DataFrame(sumple_imp.fit_transform(data[col_num]), columns = data[col_num].columns)
+    return res2
+
+  def _initial_missing_categorical_value(self, data, col_cat):
+    sumple_imp = SimpleImputer(missing_values = np.nan, strategy = 'most_frequent')
+    res1 = pd.DataFrame(sumple_imp.fit_transform(data[col_cat]), columns = data[col_cat].columns)
+    return res1
+
+  def _train_test_val_split(self, data, i, mask):
+    test = data[mask[i]]
+    train = data.drop(test.index, axis = 0)
+
+    y_test = test[i].reset_index(drop = True)
+    y_train = train[i]
+    X_test = test.drop([i], axis = 1)
+    X_train = train.drop([i], axis = 1)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size = 0.10)
+    return y_test, y_train, y_val, X_test, X_train, X_val
+  
+  def _fit_predict_model(self, model, X_train, y_train, X_test, y_test, X_val, y_val, cat_features, flag):
+    val_pool = Pool(X_val, y_val, cat_features = cat_features)
+    if flag:
+      model.fit(X_train, y_train, eval_set = val_pool, cat_features = cat_features)
+      y_test = model.predict(X_test)
+      y_test = np.reshape(y_test, (len(y_test)))
+      #error_class = model.best_score_['validation']['MultiClass']       #class error
+      error_class = 1 - accuracy_score(y_val, model.predict(X_val))
+      return error_class, y_test
+    else:
+      model.fit(X_train, y_train, eval_set = val_pool, cat_features = cat_features)
+      y_test = model.predict(X_test)
+      #error_reg = model.best_score_['validation']['RMSE'] / np.std(y_val) #reg error
+      error_reg = np.sqrt(np.mean((np.array(model.predict(X_val)) - np.array(y_val)) ** 2) / np.var(np.array(y_val)))
+      return error_reg, y_test
+
+  def print_loss(self):
+    # create figure and axis objects with subplots()
+    fig, ax = plt.subplots()
+    # make a plot
+    ax.plot(self.iter_list, self.reg_score_list, color = "red", marker = "o")
+    # set x-axis label
+    ax.set_xlabel("iter", fontsize = 14)
+    # set y-axis label
+    ax.set_ylabel("NRMSE", color = "red",fontsize = 14)
+
+    # twin object for two different y-axis on the sample plot
+    ax2 = ax.twinx()
+    # make a plot with different y-axis using second axis object
+    ax2.plot(self.iter_list, self.class_score_list, color = "blue", marker = "o")
+    ax2.set_ylabel("PFC", color="blue", fontsize = 14)
+    plt.show()
+
+  def _fit(self, X, cat_vars = None):
+    pass
+
+  def _transform(self, X):
+    pass
+  
+  def fit_transform(self, data, cat_vars = None):
+    X = data.copy()
+    # First replace missing values with NaN if it is something else
+    if self.missing_values not in ['NaN', np.nan]:
+      X[np.where(X == self.missing_values)] = np.nan
+    
+    # Check if any column has all missing
+    mask = X.isna()
+    if np.any(mask.sum(axis=0) >= (X.shape[0])):
+      raise ValueError("One or more columns have all rows missing.")
+    
+    # Создаём список имен категориальных признаков
+    self.cat_vars_ = X.columns[cat_vars] if cat_vars != None else []
+    
+    # Identify numerical variables
+    num_vars = np.setdiff1d(np.arange(X.shape[1]), cat_vars)
+    self.num_vars_ = X.columns[num_vars] if len(num_vars) > 0 else []
+
+    # Now, make initial guess for missing values
+    X = self._initial_missing_value(X, cat_vars)
+
+    model_reg = CatBoostRegressor(verbose = False, iterations = self.n_estimators, loss_function = self.criterion[0], use_best_model = True, early_stopping_rounds = 50)  # Инициализируем модели задествованные в восстановление пропущенных значений
+    model_class = CatBoostClassifier(verbose = False, iterations = self.n_estimators, loss_function = self.criterion[1], use_best_model = True, early_stopping_rounds = 50) # Инициализируем модели задествованные в восстановление пропущенных значений
+
+    err_all = np.inf # Стартовая позиция для ранней остановки, общая ошибка равна бесконечности
+
+    buff_data = X.copy()  # Сохраняем заполненный набор данных с предыдущей итерации
+    iter = 1
+
+    while iter <= self.max_iter:
+      error_class = 0
+      error_reg = 0
+
+      for i in X.columns:
+        y_test, y_train, y_val, X_test, X_train, X_val = self._train_test_val_split(X, i, mask)
+        list_cf = list([X_train.columns.get_loc(c) for c in self.cat_vars_ if c in X_train])
+
+        if i in self.cat_vars_:
+          c_err, y_test = self._fit_predict_model(model_class, X_train, y_train, X_test, y_test, X_val, y_val, list_cf, flag = True)
+          error_class = error_class + c_err
+        else:
+          r_err, y_test = self._fit_predict_model(model_reg, X_train, y_train, X_test, y_test, X_val, y_val, list_cf, flag = False)
+          error_reg = error_reg + r_err
+
+        X[i][mask[i]] = y_test
+      
+      self.iter_list.append(iter)
+      self.reg_score_list.append(error_reg)
+      self.class_score_list.append(error_class)
+
+      if (error_reg + error_class) < err_all:
+        print({'past_iter': err_all, 'present_iter': (error_reg + error_class)})
+        err_all = error_reg + error_class
+        buff_data = X.copy()
+      else:
+        print('***Aborting iterations*** ', {'past_iter': err_all, 'present_iter': (error_reg + error_class)})
+        return buff_data
+
+      iter = iter + 1
+    
+    return X
